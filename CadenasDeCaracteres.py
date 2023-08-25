cf367209ed8f27793d98f74d458b472c7d5faa1c
Daniel Sanchez <daniel.sanchez@apriorins.com>
2023-08-25 08:54:45 -0500

    Trabajando con una cadena de caracteres.

---
+++ CadenasDeCaracteres.py
@@ -0,0 +1,7 @@
+# Definimos una varaible string con el nombre
+nombre = "Daniel"
+segundaLetra = nombre[1]
+
+print("La segunda letra del nombre es " + segundaLetra)
+
+print("El nombre tiene " + str(len(nombre)) + " letras")

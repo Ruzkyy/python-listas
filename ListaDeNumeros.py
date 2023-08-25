25da2ff6ad3c25cca2cb86a61a015efd52e7443b
Daniel Sanchez <daniel.sanchez@apriorins.com>
2023-08-25 08:53:49 -0500

    Trabajando con una lista dinámica de números.

---
+++ ListaDeNumeros.py
@@ -0,0 +1,15 @@
+# LISTA dinámica, NO le especificamos elmentos
+numeros = []
+
+# Agregamos todos los elementos que queramos
+numeros.append(8)     # Se asigna a la posición 0
+numeros.append(10)    # Se asigna a la posición 1
+numeros.append(5)     # Se asigna a la posición 2
+numeros.append(4)     # Se asigna a la posición 3
+numeros.append(2)     # Se asigna a la posición 4
+
+# Imprimimos el elemento en la posición 2
+print("El elemento en la posición 1 es: " + str(numeros[1]))
+
+# Imprimimos el tamaño de la LISTA
+print("El arreglo tiene " + str(len(numeros)) + " elementos")
